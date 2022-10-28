78ace01161b5766d2a1fafc9810fc82d6b08cae4
Akshaygb <107698545+Akshaygb@users.noreply.github.com>
2022-10-28 19:52:39 +0530

    Update Numpy special fun.py.1
    I have changed sin to cos in line 4

--- Numpy special fun.py
+++ Numpy special fun.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 x=np.arange(0,3*np.pi,0.1)
-y=np.sin(x)                #i have changed tan to sin
+y=np.cos(x)                #i have changed sin to cos
 plt.plot(x,y)
 plt.show()
